9f2b09f587763ba96df27fe7f86f7e50b8b89fad
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2023-04-20 15:06:43 -0300

    Create database.java

---
+++ Outro/database.java
@@ -0,0 +1,35 @@
+import java.sql.*;
+
+public class Teste {
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.postgresql.Driver");
+        }
+        catch (java.lang.ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        String url = "jdbc:postgresql://babar.db.elephantsql.com:5432/jwovwopv";
+        String username = "jwovwopv";
+        String password = "SgL55s3Cvm9TAhQibdo7QxDG-b4EUvXB";
+
+        try {
+            Connection db = DriverManager.getConnection(url, username, password);
+            Statement st = db.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM professor");
+            while (rs.next()) {
+                System.out.print("Column 1 returned ");
+                System.out.println(rs.getString(2));
+                System.out.print("Column 2 returned ");
+                System.out.println(rs.getString(3));
+            }
+            rs.close();
+            st.close();
+            }
+        catch (java.sql.SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
