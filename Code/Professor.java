31a6a0b802d55c9863000e534273485d07e00152
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2023-04-29 10:36:44 -0300

    Update Professor.java

--- Code/Professor.java
+++ Code/Professor.java
@@ -16,20 +16,54 @@ public class Professor{
         this.cpf = cpf;
         this.formacao = formacao;
         this.email = email;
+    }
+    
+    public void Cadastrar(){
 
-        try (Connection connection = PostgreSQLConnection.getInstance().getConnection()){
+        if (buscarProfessor(this.cpf) != null) {
+            try (Connection connection = PostgreSQLConnection.getInstance().getConnection()){
 
-            PreparedStatement pstmt = connection.prepareStatement("insert into professor(nome, cpf, formacao, email) values (?, ?, ?, ?)");
-            pstmt.setString(1, nome);
-            pstmt.setString(2, cpf);
-            pstmt.setString(3, formacao);
-            pstmt.setString(4, email);
-            pstmt.executeUpdate();
+                PreparedStatement pstmt = connection.prepareStatement("insert into professor(nome, cpf, formacao, email) values (?, ?, ?, ?)");
+                pstmt.setString(1, nome);
+                pstmt.setString(2, cpf);
+                pstmt.setString(3, formacao);
+                pstmt.setString(4, email);
+                pstmt.executeUpdate();
 
             }
-        catch (java.sql.SQLException e) {
+            catch (java.sql.SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        } else{
+            System.out.println("Professor já cadastrado!");
+        }
+
+        
+
+    }
+
+    public Professor buscarProfessor(String cpf){
+        
+        try(Connection connection = PostgreSQLConnection.getInstance().getConnection()){
+
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery("SELECT cpf from professor");
+
+            while (rs.next()) {
+
+                if (cpf == rs.getString(3)) {
+                    return new Professor(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5));
+                } else{
+                    return new Professor(null, null, null, null);
+                }
+                
+            }
+
+        }
+        catch(java.sql.SQLException e){
             System.out.println(e.getMessage());
         }
+
     }
 
     /**
