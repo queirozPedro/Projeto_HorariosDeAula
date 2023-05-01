6a494763f15f87434075b88cf711aa638063657a
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2023-05-01 11:45:37 -0300

    Métodos cadastrar, buscar, excluir e listar componentes implementados

--- Code/ComponenteCurricular.java
+++ Code/ComponenteCurricular.java
@@ -1,21 +1,107 @@
+import java.sql.*;
+import java.util.ArrayList;
+
 public class ComponenteCurricular {
     private int cargaHoraria; 
     private String nome;
-    private String semestre;
+    private int semestre;
+    private String codigo;
     private boolean optativa;
 
-    public ComponenteCurricular(int cargaHoraria, String nome, String semestre, boolean optativa) {
+    public ComponenteCurricular(int cargaHoraria, String nome, int semestre, String codigo, boolean optativa) {
         this.cargaHoraria = cargaHoraria;
         this.nome = nome;
         this.semestre = semestre;
+        this.codigo = codigo;
         this.optativa = optativa;
     }
 
-    public void editaComponenteCurricular(int cargaHoraria, String nome, String semestre, boolean optativa){
-        this.cargaHoraria = cargaHoraria;
-        this.nome = nome;
-        this.semestre = semestre;
-        this.optativa = optativa;
+    public void Cadastrar(){
+        Connection connection = PostgreSQLConnection.getInstance().getConnection();
+    
+        try {
+            if (buscarComponente(this.codigo) == null) {
+                PreparedStatement pstmt = connection.prepareStatement("insert into componente_curricular(nome, carga_horaria, semestre, codigo, optativa) values (?, ?, ?, ?, ?)");
+                pstmt.setString(1, this.nome);
+                pstmt.setInt(2, this.cargaHoraria);
+                pstmt.setInt(3, this.semestre);
+                pstmt.setString(4, this.codigo);
+                pstmt.setBoolean(5, this.optativa);
+                pstmt.executeUpdate();
+                System.out.println("Componente curricular cadastrado com sucesso!");
+            } else {
+                System.out.println("Componente curricular já cadastrado!");
+            }
+        } catch (java.sql.SQLException e) {
+            System.out.println("Erro ao cadastrar componente curricular: " + e.getMessage());
+        }
+    }
+
+    public static void ExcluirComponente(String codigo){
+        Connection connection = PostgreSQLConnection.getInstance().getConnection();
+        PreparedStatement pstmt = null;
+
+        try {
+            pstmt = connection.prepareStatement("DELETE from componente_curricular where codigo = ?");
+            pstmt.setString(1, codigo);
+            int rowsDeleted = pstmt.executeUpdate();
+    
+            if (rowsDeleted > 0){
+                System.out.println("Componente curricular excluído com sucesso!");
+            }
+            else {
+                System.out.println("Componente curricular não encontrado!");
+            }
+    
+        } catch (SQLException e) {
+            System.out.println("Erro ao excluir componente curricular: " + e.getMessage());
+        }
+    }
+
+    public static ComponenteCurricular buscarComponente(String codigo){
+        Connection connection = PostgreSQLConnection.getInstance().getConnection();
+        ResultSet rs = null;
+        PreparedStatement pstmt = null;
+    
+        try{
+            pstmt = connection.prepareStatement("SELECT * from componente_curricular WHERE codigo = ?");
+            pstmt.setString(1, codigo);
+            rs = pstmt.executeQuery();
+    
+            if (rs.next()) {
+                return new ComponenteCurricular(rs.getInt(3), rs.getString(2), rs.getInt(4), rs.getString(5), rs.getBoolean(6));
+            }
+    
+            return null;
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    
+        return null;
+    }
+
+    public static ArrayList<ComponenteCurricular> listarComponentes(){
+
+        Connection connection = PostgreSQLConnection.getInstance().getConnection();
+        ArrayList<ComponenteCurricular> componentes = new ArrayList<>();
+        ResultSet rs = null;
+        PreparedStatement pstmt = null;
+    
+        try{
+            pstmt = connection.prepareStatement("SELECT * from componente_curricular");
+            rs = pstmt.executeQuery();
+    
+            while (rs.next()) {
+                ComponenteCurricular componente = new ComponenteCurricular(rs.getInt(3), rs.getString(2), rs.getInt(4), rs.getString(5), rs.getBoolean(6));
+                componentes.add(componente);
+            }
+    
+            return componentes;
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        
+        return componentes;
     }
 
     @Override
