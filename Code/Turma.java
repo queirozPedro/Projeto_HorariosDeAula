b5f9a919bf4215437c89e1946791c1e5799d12dd
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2023-05-05 18:19:38 -0300

    Tratando dados
    Não será possível adicionar turmas do mesmo semestre com o mesmo horário e não será possível adicionar turmas que são ministradas pelo mesmo professor no mesmo horário

--- Code/Turma.java
+++ Code/Turma.java
@@ -1,6 +1,5 @@
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Turma{
     private int idTurma;
@@ -35,19 +34,50 @@ public class Turma{
         return idProfessor;
     }
 
+    public int getIdTurma() {
+        return idTurma;
+    }
+
     public void Cadastrar(){
 
         Connection connection = PostgreSQLConnection.getInstance().getConnection();
+        ResultSet rs = null;
+        PreparedStatement pstmt = null;
     
         try {
 
-            PreparedStatement pstmt = connection.prepareStatement("SELECT count(*) FROM turma WHERE id_comp = ?");
+            pstmt = connection.prepareStatement("SELECT count(*) FROM turma WHERE id_comp = ?");
             pstmt.setInt(1, this.idComponenteCurricular);
-            ResultSet rs = pstmt.executeQuery();
+            rs = pstmt.executeQuery();
 
             if (rs.next()) {
                 this.codigo += rs.getInt(1);
             }
+
+            pstmt = connection.prepareStatement("SELECT horario1, horario2 FROM turma as t INNER JOIN componente_curricular as cc ON t.id_comp = cc.id_comp WHERE semestre in(SELECT semestre FROM componente_curricular WHERE id_comp = ?)");
+            pstmt.setInt(1, this.idComponenteCurricular);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                if (this.horario1.equals(rs.getString(1)) || (this.horario2 != null && this.horario2.equals(rs.getString(1))) ||this.horario1.equals(rs.getString(2)) || (this.horario2 != null && this.horario2.equals(rs.getString(2)))) {
+                System.out.println("Horário não está disponível!");
+                return;
+                }
+
+            }
+
+            pstmt = connection.prepareStatement("SELECT horario1, horario2 FROM turma NATURAL JOIN turma_professor WHERE id_prof = ?");
+            pstmt.setInt(1, this.idProfessor.get(0));
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                if (this.horario1.equals(rs.getString(1)) || (this.horario2 != null && this.horario2.equals(rs.getString(1))) ||this.horario1.equals(rs.getString(2)) || (this.horario2 != null && this.horario2.equals(rs.getString(2)))) {
+                System.out.println("Horário não está disponível!");
+                return;
+                }
+
+            }
+
             pstmt = connection.prepareStatement("INSERT INTO turma (id_comp, horario1, horario2, vagas, codigo) VALUES (?, ?, ?, ?, ?)");
             pstmt.setInt(1, this.idComponenteCurricular);
             pstmt.setString(2, this.horario1);
@@ -79,17 +109,29 @@ public class Turma{
 
     public static void ExcluirTurma(int id_turma){
         Connection connection = PostgreSQLConnection.getInstance().getConnection();
+        ResultSet rs = null;
         PreparedStatement pstmt = null;
 
         try {
-            pstmt = connection.prepareStatement("DELETE FROM turma WHERE id_turma = ?");
+
+            pstmt = connection.prepareStatement("SELECT codigo, id_comp FROM turma WHERE id_turma = ?");
             pstmt.setInt(1, id_turma);
-            int rowsDeleted = pstmt.executeUpdate();
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                pstmt = connection.prepareStatement("UPDATE turma SET codigo = codigo - 1 WHERE codigo > ? AND id_comp = ?");
+                pstmt.setInt(1, rs.getInt(1));
+                pstmt.setInt(2, rs.getInt(2));
+                pstmt.executeUpdate();
+            }
 
             pstmt = connection.prepareStatement("DELETE FROM turma_professor WHERE id_turma = ?");
             pstmt.setInt(1, id_turma);
             pstmt.executeUpdate();
     
+            pstmt = connection.prepareStatement("DELETE FROM turma WHERE id_turma = ?");
+            pstmt.setInt(1, id_turma);
+            int rowsDeleted = pstmt.executeUpdate();
             if (rowsDeleted > 0){
                 System.out.println("Turma excluída com sucesso!");
             }
@@ -105,10 +147,40 @@ public class Turma{
     public static void adicionarProfessor(int id_turma, int id_prof){
 
         Connection connection = PostgreSQLConnection.getInstance().getConnection();
+        PreparedStatement pstmt = null;
+        PreparedStatement pstmt2 = null;
+        ResultSet rs = null;
+        ResultSet rs2 = null;
     
         try {
 
-            PreparedStatement pstmt = connection.prepareStatement("INSERT INTO turma_professor (id_turma, id_prof) VALUES (?, ?)");
+            pstmt = connection.prepareStatement("SELECT horario1, horario2 FROM turma WHERE id_turma = ?");
+            pstmt.setInt(1, id_turma);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+
+                pstmt2 = connection.prepareStatement("SELECT horario1, horario2 FROM turma NATURAL JOIN turma_professor WHERE id_prof = ?");
+                pstmt2.setInt(1, id_prof);
+                rs2 = pstmt2.executeQuery();
+
+                while (rs2.next()) {
+                    String horario1_turma = rs.getString(1);
+                    String horario2_turma = rs.getString(2);
+                    String horario1_prof = rs2.getString(1);
+                    String horario2_prof = rs2.getString(2);
+
+                    if (horario1_turma.equals(horario1_prof) || (horario2_turma != null && horario2_turma.equals(horario1_prof)) || horario1_turma.equals(horario2_prof) || (horario2_turma != null && horario2_turma.equals(horario2_prof))) {
+                    System.out.println("Horário não está disponível!");
+                    return;
+                    }
+                }
+
+                
+
+            }
+
+            pstmt = connection.prepareStatement("INSERT INTO turma_professor (id_turma, id_prof) VALUES (?, ?)");
             pstmt.setInt(1, id_turma);
             pstmt.setInt(2, id_prof);
             pstmt.executeUpdate();
@@ -257,21 +329,49 @@ public class Turma{
         Connection connection = PostgreSQLConnection.getInstance().getConnection();
         ArrayList<Turma> turmas = new ArrayList<>();
         ResultSet rs = null;
+        ResultSet rs2 = null;
         PreparedStatement pstmt = null;
+        PreparedStatement pstmt2 = null;
     
         try{
-            pstmt = connection.prepareStatement("SELECT id_turma FROM turma as t INNER JOIN componente_curricular as cc on t.id_comp = cc.id_comp WHERE semestre = ? ORDER BY id_turma");
+            pstmt = connection.prepareStatement("SELECT t.id_turma, t.id_comp, t.horario1, t.horario2, t.vagas, t.codigo FROM turma as t INNER JOIN componente_curricular as cc on t.id_comp = cc.id_comp WHERE semestre = ? ORDER BY id_turma");
             pstmt.setInt(1, semestre);
             rs = pstmt.executeQuery();
+
+            int id_turma = 0;
+            int id_comp = 0;
+            String horario1 = "";
+            String horario2 = "";
+            int vagas = 0;
+            int numero_turma = 0;
+            ArrayList<Integer> professores = new ArrayList<>();
     
             while (rs.next()) {
 
-                Turma turma = Turma.buscarTurma(rs.getInt(1));
+                id_turma = rs.getInt(1);
+                id_comp = rs.getInt(2);
+                horario1 = rs.getString(3);
+                horario2 = rs.getString(4);
+                vagas = rs.getInt(5);
+                numero_turma = rs.getInt(6);
+
+                professores = new ArrayList<>();
+
+                pstmt2 = connection.prepareStatement("SELECT id_prof FROM turma_professor WHERE id_turma = ? ORDER BY id_prof");
+                pstmt2.setInt(1, rs.getInt(1));
+                rs2 = pstmt2.executeQuery();
+
+                while (rs2.next()) {
+                    professores.add(rs2.getInt(1));
+                }
+
+                Turma turma = new Turma(id_turma, professores, id_comp, horario1, horario2, vagas, numero_turma);
                 turmas.add(turma);
+
             }
     
             return turmas;
-        } catch(SQLException e){
+        }catch(SQLException e){
             System.out.println(e.getMessage());
         }
         
@@ -287,26 +387,41 @@ public class Turma{
         PreparedStatement pstmt = null;
     
         try{
-            pstmt = connection.prepareStatement("SELECT * FROM turma WHERE ? = ANY(id_prof)");
+            pstmt = connection.prepareStatement("SELECT * FROM turma NATURAL JOIN turma_professor WHERE id_prof = ?");
             pstmt.setInt(1, id_prof);
             rs = pstmt.executeQuery();
+
+            int id_turma = 0;
+            int id_comp = 0;
+            String horario1 = "";
+            String horario2 = "";
+            int vagas = 0;
+            int numero_turma = 0;
+            ArrayList<Integer> professores = new ArrayList<>();
     
             while (rs.next()) {
-                Array array = rs.getArray("id_prof");
-                Integer[] intArray = (Integer[]) array.getArray();
-                ArrayList<Integer> professores = new ArrayList<>(Arrays.asList(intArray));
 
-                Turma turma = new Turma(rs.getInt(1), professores, rs.getInt(2), rs.getString(3), rs.getString(4), rs.getInt(5), rs.getInt(6));
+                professores = new ArrayList<>();
+
+                id_turma = rs.getInt(1);
+                id_comp = rs.getInt(2);
+                horario1 = rs.getString(3);
+                horario2 = rs.getString(4);
+                vagas = rs.getInt(5);
+                numero_turma = rs.getInt(6);
+                professores.add(rs.getInt(7));;
+
+                Turma turma = new Turma(id_turma, professores, id_comp, horario1, horario2, vagas, numero_turma);
                 turmas.add(turma);
+
             }
     
             return turmas;
-        } catch(SQLException e){
+        }catch(SQLException e){
             System.out.println(e.getMessage());
         }
         
         return turmas;
-
     }
 
     public static void editaTurma(int id_turma, Object dado, int id_dado, int id_prof){
